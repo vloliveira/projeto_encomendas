380097ce4b67b455f883f3e0f82dd0d16243bfad
vloliveira <vitorloliveira96@gmail.com>
2025-09-03 15:51:02 -0300

    fix: correção de bug de conexão com o banco de dados

--- encomendas/src/main/java/com/chegouAqui/encomendas/EncomendasApplication.java
+++ encomendas/src/main/java/com/chegouAqui/encomendas/EncomendasApplication.java
@@ -4,8 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-// ATENÇÃO: Remover "(exclude = {DataSourceAutoConfiguration.class})" para rodar o banco de dados
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class EncomendasApplication {
 
 	public static void main(String[] args) {
