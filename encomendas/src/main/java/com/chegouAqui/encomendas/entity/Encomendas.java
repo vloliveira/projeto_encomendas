6e350561cebd9da12a4115e13286bb93fcf1b67a
vloliveira <vitorloliveira96@gmail.com>
2025-09-09 17:29:54 -0300

    feat: inclusão do package entity com getters e setters de cada entidade

---
+++ encomendas/src/main/java/com/chegouAqui/encomendas/entity/Encomendas.java
@@ -0,0 +1,36 @@
+package com.chegouAqui.encomendas.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "encomendas")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Encomendas {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String status;
+    private LocalDateTime data_recebimento;
+    private LocalDateTime data_entrega;
+    private String pin;
+    private String tipo;
+
+    @ManyToOne
+    @JoinColumn(name = "fk_id_morador")
+    private Morador morador;
+
+    @ManyToOne
+    @JoinColumn(name = "fk_id_porteiro")
+    private Porteiro porteiro;
+}
